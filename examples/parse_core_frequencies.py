d1e3c065cf4fcfe7cea0cf1ce542afd83626234f
Chris <dreamtale.jg@gmail.com>
2020-03-23 18:39:50 +0800

    add process cpu usage

--- examples/parse_core_frequencies.py
+++ examples/parse_core_frequencies.py
@@ -14,6 +14,7 @@ ALL_CPUS = LITTLE_CPUS.union(BIG_CPUS)
 FREQ_ALL_CORES =[300000,576000 ,614400, 864000, 1075200 ,1363200, 1516800, 1651200 ,1804800,652800,  940800,  1152000,  1478400,  1728000,  1900800 , 2092800,  2208000, 806400,  1094400 , 1401600 , 1766400,  1996800 , 2188800,  2304000 , 2400000]
 task_tid = []
 
+print 'parse argument start'
 parser = argparse.ArgumentParser(description='Per-core frequencies')
 
 parser.add_argument('-f', '--file', dest='file',
@@ -22,23 +23,51 @@ parser.add_argument('-pf', '--process file', dest='process_file',
                     help='Process file to parse')
 args = parser.parse_args()
 
+print 'parse argument end'
+print 'parse trace start'
 trace = Ftrace(args.file)
+print 'parse trace end'
+print 'parse process info start'
 process_info = ProcessInfoParse()
-process_info.parse_process_info(args.process_file)
+lsit = process_info.parse_process_info(args.process_file)
+task_tid_list = process_info.get_all_task_tid()
+print 'parse process info end'
 
+print 'cpu.task_intervals start'
 task_interval = trace.cpu.task_intervals()
 task_tid_list = process_info.get_all_task_tid()
-print task_tid_list
+print 'cpu.task_intervals end'
+
+print 'main parse start'
 for busy_interval_item in task_interval:
-    for task_tid in task_tid_list:
-        if busy_interval_item.task.pid == int(task_tid) and busy_interval_item.state == TaskState.RUNNING:
-            pid = process_info.get_pid(task_tid)
-            process_info.save_running_info_to_tid_list(pid,task_tid,busy_interval_item.task.name ,busy_interval_item.interval.duration)
-            process_info.save_running_info_to_pid_list(pid,task_tid,busy_interval_item.task.name ,busy_interval_item.interval.duration)
-            break
+    if busy_interval_item.state == TaskState.RUNNING:
+        task_tid = busy_interval_item.task.pid
+        task_pid = process_info.get_pid(task_tid)
+        task_name = process_info.get_taskname_by_tid(task_tid)
+        process_info.save_running_info_to_pid_list(task_pid,task_tid,task_name ,busy_interval_item.interval.duration)
+print 'main parse end'
+    # for task_tid in task_tid_list:
+    #     if busy_interval_item.task.pid == int(task_tid) and busy_interval_item.state == TaskState.RUNNING:
+    #         pid = process_info.get_pid(task_tid)
+    #         # process_info.save_running_info_to_tid_list(pid,task_tid,busy_interval_item.task.name ,busy_interval_item.interval.duration)
+    #         process_info.save_running_info_to_pid_list(pid,task_tid,busy_interval_item.task.name ,busy_interval_item.interval.duration)
+    #         break
 
 print "Total trace time = " + str(trace.duration)
 process_info.print_result_sort_by_pid()
 # process_info.print_result_sort_by_all()
 # process_info.print_result_sort_by_tid()
 
+print 'freq parse start'
+df_freq = DataFrame( index = ALL_CPUS, columns=FREQ_ALL_CORES)
+df_freq.fillna(0, inplace=True)
+for cpu in ALL_CPUS:
+    for busy_interval in trace.cpu.busy_intervals(cpu=cpu):
+        for freq in trace.cpu.frequency_intervals(cpu=cpu, interval=busy_interval.interval):
+            df_freq.loc[cpu, freq.frequency] += freq.interval.duration
+
+print df_freq.to_string()
+df_freq.to_csv("lock_to_launcher_2020_3_23_freq.csv")
+print 'freq parse end'
+
+print 'All done'
--- examples/parse_process.py
+++ examples/parse_process.py
@@ -73,6 +73,11 @@ class ProcessInfoParse:
       tidList.append(processInfos.tid)
     return tidList
 
+  def get_taskname_by_tid(self , tid):
+    for processInfos in processInfolist:
+      if processInfos.tid == tid:
+        return processInfos.process_name
+
   def save_running_info_to_tid_list(self , pid , tid , name , duration):
     # record tid
     if result_list_tid is not None:
@@ -81,19 +86,19 @@ class ProcessInfoParse:
           dic['duration']+= duration
           dic['name'] = name
           return
-    dics = {'pid' : pid, 'tid' : tid ,'name': name , 'duration' : duration}
+    dics = {'pid' : pid, 'tid' : tid ,'name': name , 'duration' : duration * 1000}
     result_list_tid.append(dics)
 
   def save_running_info_to_pid_list(self , pid , tid , name , duration):
     # record pid
     if result_list_pid is not None:
       for dic in result_list_pid:
-        if dic['pid'] == pid:
+        if tid == pid:
           dic['duration'] += duration
-          if dic['pid'] == tid:
-            dic['name'] = name
           return
-    dic_pid = {'pid' : pid,'name': name , 'duration' : duration}
+    
+    #pid_name = self.get_taskname_by_tid(tid)
+    dic_pid = {'pid' : pid,'name': name , 'duration' : duration * 1000}
     result_list_pid.append(dic_pid)
 
   def print_result_sort_by_tid(self):
@@ -103,11 +108,11 @@ class ProcessInfoParse:
 
   def print_result_sort_by_pid(self):
     print '# cpu time by pid view #'
-    for dic_pid in result_list_pid:
-      pid = dic_pid['pid']
-      print 'pid = ' + str(dic_pid['pid'])+ '  name = ' + dic_pid['name'] + ' duration = ' + str(dic_pid['duration'])
+    # for dic_pid in result_list_pid:
+    #   pid = dic_pid['pid']
+      # print 'pid = ' + str(dic_pid['pid'])+ '  name = ' + dic_pid['name'] + ' duration = ' + str(dic_pid['duration']* 1000)
     df = pd.DataFrame(result_list_pid, columns=['name','pid','duration'])
-    df.to_csv("app_lunch.csv")
+    df.to_csv("lock_to_launcher_2020_3_23_15.csv")
 
   def print_result_sort_by_all(self):
     print '# cpu time by pid and tid view #'
